3760196bddc7c8dfa5a788fdc1847ac9d15a9b67
Antony <antony.ictr@gmail.com>
2026-02-06 09:51:08 +0400

    refactor: remove `MANIFEST_FILENAME` and `RESTORE_DIR_NAME` constants and add `APP_NAME_FOR_CACHE`.

--- src/constants.rs
+++ src/constants.rs
@@ -3,12 +3,7 @@ pub const DEFAULT_ZSTD_COMPRESSION: u32 = 19;
 
 /// Application name for directory naming (XDG_CACHE_HOME, etc.)
 pub const APP_NAME: &str = "zero-kelvin-stazis";
-
-/// Filename for the manifest inside the SquashFS image
-pub const MANIFEST_FILENAME: &str = "list.yaml";
-
-/// Directory name inside the SquashFS image where files are stored
-pub const RESTORE_DIR_NAME: &str = "to_restore";
+pub const APP_NAME_FOR_CACHE: &str = "stazis";
 
 /// Size of the LUKS2 header in bytes
 pub const LUKS_HEADER_SIZE: u64 = 32 * 1024 * 1024; // 32MB for LUKS2 header
